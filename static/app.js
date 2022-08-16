feb48c7ccf5a7689ac003f64092d08520c41b2e1
Ahmed Ibrahim <ib153507@gmail.com>
2022-08-16 07:00:46 +0200

    Add infinite scrolling

--- static/app.js
+++ static/app.js
@@ -5,24 +5,57 @@ const state = {
   pageSize: 12,
 };
 
-main();
+// main();
 
-async function main() {
-  content.innerHTML = `
-    <div class="w-full flex items-center justify-center col-span-12">
-        <p class='text-indigo-900 text-xl font-bold'>Processing...</p>
-    </div>
-    `;
+// async function main() {
+//   content.innerHTML = `
+//     <div class="w-full flex items-center justify-center col-span-12">
+//         <p class='text-indigo-900 text-xl font-bold'>Processing...</p>
+//     </div>
+//     `;
 
-  try {
-    const result = await fetch(getUrl()).then((res) => res.json());
+//   try {
+//     const result = await fetch(getUrl()).then((res) => res.json());
 
-    console.log(result);
-    let html = "";
+//     console.log(result);
+//     let html = "";
 
-    result.urls.forEach((url) => {
-      html += `
-        <div class="col-span-12 sm:col-span-6 md:col-span-4 xl:col-span-3 bg-gray-100">
+//     result.urls.forEach((url) => {
+//       html += `
+//         <div class="col-span-12 sm:col-span-6 md:col-span-4 xl:col-span-3 bg-gray-100">
+//             <img
+//                 src="${url}"
+//                 alt="Pokemon"
+//                 title="Pokemon"
+//                 loading="lazy"
+//             />
+//         </div>
+//         `;
+//     });
+
+//     content.innerHTML = html;
+//   } catch (error) {
+//     alert("Error getting pokemons from the server. please retry");
+//   }
+// }
+
+// function getUrl() {
+//   const { page, pageSize } = state;
+//   return `http://localhost:8080/api/v1/pokemon?page=${page}&page_size=${pageSize}`;
+// }
+
+let infScroll = new InfiniteScroll("#content", {
+  path: `http://localhost:8080/api/v1/pokemon?page={{#}}&page_size=12`,
+  responseBody: "json",
+  history: false,
+});
+
+infScroll.on("load", function (data) {
+  let html = "";
+
+  data.urls.forEach((url) => {
+    html += `
+        <div class="col-span-6 md:col-span-4 xl:col-span-3 bg-gray-100">
             <img
                 src="${url}"
                 alt="Pokemon"
@@ -31,15 +64,8 @@ async function main() {
             />
         </div>
         `;
-    });
+  });
+  content.innerHTML += html;
+});
 
-    content.innerHTML = html;
-  } catch (error) {
-    alert("Error getting pokemons from the server. please retry");
-  }
-}
-
-function getUrl() {
-  const { page, pageSize } = state;
-  return `http://localhost:8080/api/v1/pokemon?page=${page}&page_size=${pageSize}`;
-}
+infScroll.loadNextPage();
