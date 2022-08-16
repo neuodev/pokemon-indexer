18e1759b34ad92fe19acf922a932bcb51ceab627
Ahmed Ibrahim <ib153507@gmail.com>
2022-08-16 04:56:29 +0200

    Save all urls to json file

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,29 @@
 mod pokemon;
-use colored::Colorize;
 use clap::Parser;
+use colored::Colorize;
 
 const NUM_OF_PAGES: u8 = 252;
 const PAGES_TO_PROCESS: u8 = 5;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
-struct Args {
+pub struct Args {
     #[clap(short, long, value_parser, default_value_t = 5)]
     pages: u8,
     #[clap(short, long, value_parser, default_value_t = false)]
     json: bool,
     #[clap(short, long, value_parser, default_value_t = true)]
-    save_images: bool
+    save_images: bool,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let total_iters =  NUM_OF_PAGES / PAGES_TO_PROCESS;
+    let args = Args::parse();
+
+    let total_iters = NUM_OF_PAGES / args.pages;
     println!("Total iterations: {}", total_iters);
 
-    for i in 1..=total_iters {        
+    for i in 1..=total_iters {
         let offset = ((i - 1) * PAGES_TO_PROCESS) + 1;
         let mut handlers = vec![];
         println!("[chunk]: {}", format!("{}", i).bold().underline().yellow());
@@ -29,26 +31,31 @@ async fn main() -> Result<(), reqwest::Error> {
             let handler = tokio::spawn(async move {
                 match pokemon::fetch_page(page).await {
                     Ok(result) => {
-                        println!("[page]: {}", format!("{}", page ).bold().underline().cyan());
+                        println!("[page]: {}", format!("{}", page).bold().underline().cyan());
                         let urls = pokemon::extract_img_urls(result.as_str());
                         pokemon::save_page_images(urls, page).await;
                         println!("[done]: {}", format!("{}", page).bold().underline().green());
-                    },
+                    }
                     Err(e) => {
-                        println!("{}", format!("Error processing page '{}'", page).bold().underline().red());
+                        println!(
+                            "{}",
+                            format!("Error processing page '{}'", page)
+                                .bold()
+                                .underline()
+                                .red()
+                        );
                         println!("{e}");
                     }
                 }
             });
 
             handlers.push(handler);
-        } 
+        }
 
         for handler in handlers {
             handler.await.unwrap();
         }
     }
-
 
     Ok(())
 }
--- src/pokemon.rs
+++ src/pokemon.rs
@@ -1,12 +1,13 @@
 use colored::Colorize;
 use select::document::Document;
-use select::predicate::{Class};
-// use std::io;
-// use std::fs::File;
-use tokio::fs::{File, self};
-use tokio::io;
+use select::predicate::Class;
+use tokio::fs::{self, File};
+use tokio::io::{self, AsyncWriteExt};
 
-/// Fetch the HTML per page 
+const NUM_OF_PAGES: u8 = 252;
+const OUTPUT_JSON_FILE: &str = "./ouptut/urls.json";
+
+/// Fetch the HTML per page
 /// There is about 252 page on this website https://pkmncards.com/page/1/?s with over more than 15,000 image
 pub async fn fetch_page(num: u8) -> Result<String, reqwest::Error> {
     let response = reqwest::get(format!("https://pkmncards.com/page/{}/?s", num)).await?;
@@ -17,12 +18,18 @@ pub async fn fetch_page(num: u8) -> Result<String, reqwest::Error> {
 pub fn extract_img_urls(html: &str) -> Vec<String> {
     let doc = Document::from(html);
     let image_elements = doc.find(Class("card-image"));
-    let urls = image_elements.into_iter().filter_map(|n| n.attr("src")).map(|src| src.to_string()).collect::<Vec<String>>();
+    let urls = image_elements
+        .into_iter()
+        .filter_map(|n| n.attr("src"))
+        .map(|src| src.to_string())
+        .collect::<Vec<String>>();
     urls
 }
 
 pub async fn save_page_images(urls: Vec<String>, page: u8) {
-    fs::create_dir_all(format!("./output/{page}").as_str()).await.unwrap();
+    fs::create_dir_all(format!("./output/{page}").as_str())
+        .await
+        .unwrap();
 
     let mut handlers = vec![];
     for (idx, url) in urls.into_iter().enumerate() {
@@ -30,14 +37,22 @@ pub async fn save_page_images(urls: Vec<String>, page: u8) {
             if let Ok(resp) = reqwest::get(url).await {
                 println!("[image fetched] [page]: {}, [image]: {}", page, idx);
                 if let Ok(bytes) = resp.bytes().await {
-                    let mut out = File::create(format!("./output/{}/{}.png", page, idx + 1)).await.unwrap(); // Todo: remove unwrap
+                    let mut out = File::create(format!("./output/{}/{}.png", page, idx + 1))
+                        .await
+                        .unwrap(); // Todo: remove unwrap
                     let mut resp_slice: &[u8] = bytes.as_ref();
-                    io::copy(&mut resp_slice, &mut out).await.expect("failed to copy content");
+                    io::copy(&mut resp_slice, &mut out)
+                        .await
+                        .expect("failed to copy content");
                 } else {
                     println!("Unable to read bytes for {idx} on page {page}");
                 }
             } else {
-                println!("[error]: [page]: {}, [image]: {}", format!("{}", page).bold().underline().red(), format!("{}", idx).bold().underline().red());
+                println!(
+                    "[error]: [page]: {}, [image]: {}",
+                    format!("{}", page).bold().underline().red(),
+                    format!("{}", idx).bold().underline().red()
+                );
             }
         });
 
@@ -49,3 +64,63 @@ pub async fn save_page_images(urls: Vec<String>, page: u8) {
     }
 }
 
+// Save all urls for all 15,000 images into a json
+async fn save_urls(urls: Vec<String>) {
+    let mut file = File::create(OUTPUT_JSON_FILE)
+        .await
+        .expect("Unable to create output json file");
+    let json = serde_json::to_string(&urls).expect("Invalid json");
+    file.write(json.as_bytes())
+        .await
+        .expect("Failed to write to the ouptut json file");
+}
+
+pub async fn pokemon_download<'a>(args: &'a crate::Args) {
+    let total_iters = NUM_OF_PAGES / args.pages;
+    println!("Total iterations: {}", total_iters);
+
+    let mut all_urls = vec![];
+
+    for i in 1..=total_iters {
+        let offset = ((i - 1) * args.pages) + 1;
+        let mut handlers = vec![];
+        println!("[chunk]: {}", format!("{}", i).bold().underline().yellow());
+        for page in offset..offset + args.pages {
+            let handler = tokio::spawn(async move {
+                match fetch_page(page).await {
+                    Ok(result) => {
+                        println!("[page]: {}", format!("{}", page).bold().underline().cyan());
+                        let urls = extract_img_urls(result.as_str());
+                        if args.save_images == true {
+                            save_page_images(urls.clone(), page).await;
+                        }
+                        println!("[done]: {}", format!("{}", page).bold().underline().green());
+                        return urls;
+                    }
+                    Err(e) => {
+                        println!(
+                            "{}",
+                            format!("Error processing page '{}'", page)
+                                .bold()
+                                .underline()
+                                .red()
+                        );
+                        println!("{e}");
+                        return vec![];
+                    }
+                }
+            });
+
+            handlers.push(handler);
+        }
+
+        for handler in handlers {
+            let mut urls = handler.await.unwrap();
+            all_urls.append(&mut urls);
+        }
+    }
+
+    if args.json == true {
+        save_urls(all_urls).await;
+    }
+}
