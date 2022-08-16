23c19bd00b014d0ffe46094ab2fd68c16ce551ca
Ahmed Ibrahim <72753578+AhmedIbrahim336@users.noreply.github.com>
2022-08-16 19:45:00 +0200

    Add `--addr` flag when running the server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "pokemon",
-      script: "./target/release/pokemon-cards.exe",
+      script: "./target/release/pokemon-cards --addr 0.0.0.0:8080",
       exec_interpreter: "none",
       exec_mode: "fork_mode",
     },
