cbab67a875f07f677fef3158e0b0b685b4b10d14
Ahmed Ibrahim <ib153507@gmail.com>
2022-08-16 06:46:09 +0200

    Display images in the html page

--- static/app.js
+++ static/app.js
@@ -1,14 +1,45 @@
 const content = document.getElementById("content");
 
+const state = {
+  page: 1,
+  pageSize: 12,
+};
+
 main();
 
 async function main() {
+  content.innerHTML = `
+    <div class="w-full flex items-center justify-center col-span-12">
+        <p class='text-indigo-900 text-xl font-bold'>Processing...</p>
+    </div>
+    `;
+
   try {
-    const result = fetch("http://localhost:8080/api/v1/pokemon").then((res) =>
-      res.json()
-    );
+    const result = await fetch(getUrl()).then((res) => res.json());
+
     console.log(result);
+    let html = "";
+
+    result.urls.forEach((url) => {
+      html += `
+        <div class="col-span-12 sm:col-span-6 md:col-span-4 xl:col-span-3 bg-gray-100">
+            <img
+                src="${url}"
+                alt="Pokemon"
+                title="Pokemon"
+                loading="lazy"
+            />
+        </div>
+        `;
+    });
+
+    content.innerHTML = html;
   } catch (error) {
-    console.log(error);
+    alert("Error getting pokemons from the server. please retry");
   }
+}
+
+function getUrl() {
+  const { page, pageSize } = state;
+  return `http://localhost:8080/api/v1/pokemon?page=${page}&page_size=${pageSize}`;
 }
